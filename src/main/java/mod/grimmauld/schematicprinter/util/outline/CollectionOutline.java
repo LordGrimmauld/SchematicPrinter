d0eefbeb6ff367a4f3d1961d7bdf8a74e90e1e6a
grimmauld <soeren@benjos.de>
2021-02-10 21:01:38 +0100

    trying to parallelize

--- src/main/java/mod/grimmauld/schematicprinter/util/outline/CollectionOutline.java
+++ src/main/java/mod/grimmauld/schematicprinter/util/outline/CollectionOutline.java
@@ -5,85 +5,64 @@ import mod.grimmauld.schematicprinter.render.SuperRenderTypeBuffer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
-import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class CollectionOutline extends Outline {
-	private final Set<BlockPos> positions;
-	private final Set<LineVecPair> edges;
-	private int upwardsExtension = 1;
+	private final ConcurrentHashMap<LineVecPair, AtomicInteger> edges;
 
 	public CollectionOutline() {
-		this.positions = new HashSet<>();
-		edges = new HashSet<>();
+		edges = new ConcurrentHashMap<>();
 	}
 
 	@Override
 	public void render(MatrixStack ms, SuperRenderTypeBuffer buffer) {
-		edges.forEach(line -> this.renderAACuboidLine(ms, buffer, line.getFirst(), line.getSecond()));
-	}
-
-	public CollectionOutline withPositions(Collection<BlockPos> positions) {
-		if (this.positions.addAll(positions))
-			recalculateVertices();
-		return this;
+		edges.forEach((line, n) -> this.renderAACuboidLine(ms, buffer, line.getFirst(), line.getSecond()));
 	}
 
 	public CollectionOutline withPositions(Stream<BlockPos> positions) {
-		AtomicBoolean changed = new AtomicBoolean(false);
-		positions.filter(this.positions::add).forEach(pos -> changed.set(true));
-		if (changed.get())
-			recalculateVertices();
+		edges.clear();
+		positions.parallel().forEach(this::addVerticeForPos);
+		edges.entrySet().stream().filter(entry -> entry.getValue().get() % 2 == 0).forEach(entry -> edges.remove(entry.getKey()));
 		return this;
 	}
 
-	private void recalculateVertices() {
-		edges.clear();
-
-		for (BlockPos pos : positions) {
-			Vec3d xyz = new Vec3d(pos.getX(), pos.getY(), pos.getZ());
-			Vec3d Xyz = new Vec3d(pos.getX() + 1, pos.getY(), pos.getZ());
-			Vec3d xYz = new Vec3d(pos.getX(), pos.getY() + 1, pos.getZ());
-			Vec3d XYz = new Vec3d(pos.getX() + 1, pos.getY() + 1, pos.getZ());
-			Vec3d xyZ = new Vec3d(pos.getX(), pos.getY(), pos.getZ() + 1);
-			Vec3d XyZ = new Vec3d(pos.getX() + 1, pos.getY(), pos.getZ() + 1);
-			Vec3d xYZ = new Vec3d(pos.getX(), pos.getY() + 1, pos.getZ() + 1);
-			Vec3d XYZ = new Vec3d(pos.getX() + 1, pos.getY() + 1, pos.getZ() + 1);
-			addVertices(xYz, Xyz, xyz, XYZ, XyZ, xyZ);
-			addVertices(Xyz, xYz, XYz, xyZ, xYZ, XYZ);
-		}
-
-		edges.stream().max(Comparator.comparing(LineVecPair::getMaxY)).map(LineVecPair::getMaxY).ifPresent(maxY -> edges.forEach(edge -> edge.extendUpwards(upwardsExtension - 1, maxY)));
+	private void addVerticeForPos(BlockPos pos) {
+		Vec3d xyz = new Vec3d(pos.getX(), pos.getY(), pos.getZ());
+		Vec3d Xyz = new Vec3d(pos.getX() + 1, pos.getY(), pos.getZ());
+		Vec3d xYz = new Vec3d(pos.getX(), pos.getY() + 1, pos.getZ());
+		Vec3d XYz = new Vec3d(pos.getX() + 1, pos.getY() + 1, pos.getZ());
+		Vec3d xyZ = new Vec3d(pos.getX(), pos.getY(), pos.getZ() + 1);
+		Vec3d XyZ = new Vec3d(pos.getX() + 1, pos.getY(), pos.getZ() + 1);
+		Vec3d xYZ = new Vec3d(pos.getX(), pos.getY() + 1, pos.getZ() + 1);
+		Vec3d XYZ = new Vec3d(pos.getX() + 1, pos.getY() + 1, pos.getZ() + 1);
+		addVertices(xYz, Xyz, xyz, XYZ, XyZ, xyZ);
+		addVertices(Xyz, xYz, XYz, xyZ, xYZ, XYZ);
 	}
 
 	private void addVertices(Vec3d xyz, Vec3d xYz, Vec3d XYz, Vec3d xyZ, Vec3d xYZ, Vec3d XYZ) {
-		addOrDeleteVertex(XYz, xYz);
-		addOrDeleteVertex(XYz, xyz);
-		addOrDeleteVertex(XYz, XYZ);
-		addOrDeleteVertex(xYZ, XYZ);
-		addOrDeleteVertex(xYZ, xyZ);
-		addOrDeleteVertex(xYZ, xYz);
+		addVertex(XYz, xYz);
+		addVertex(XYz, xyz);
+		addVertex(XYz, XYZ);
+		addVertex(xYZ, XYZ);
+		addVertex(xYZ, xyZ);
+		addVertex(xYZ, xYz);
 	}
 
-	private void addOrDeleteVertex(Vec3d from, Vec3d to) {
+	private void addVertex(Vec3d from, Vec3d to) {
 		LineVecPair pair = LineVecPair.of(from, to);
-		if (edges.contains(pair))
-			edges.remove(pair);
+		AtomicInteger n = edges.get(pair);
+		if (n != null)
+			n.incrementAndGet();
 		else
-			edges.add(pair);
+			edges.put(pair, new AtomicInteger(1));
 	}
 
 	@Override
 	public Outline extendedUpwards(int value) {
-		if (upwardsExtension != value) {
-			upwardsExtension = value;
-			recalculateVertices();
-		}
-
+		edges.keySet().stream().max(Comparator.comparing(LineVecPair::getMaxY)).map(LineVecPair::getMaxY).ifPresent(maxY -> edges.keySet().forEach(edge -> edge.extendUpwards(value - 1, maxY)));
 		return super.extendedUpwards(value);
 	}
 }
